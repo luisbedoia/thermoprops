baef211e058740189c2be39e3f7fc321d3a67cfc
Luis Arias <ariasb.luis@gmail.com>
2025-11-24 20:26:43 -0500

    Update @luisbedoia/coolprop-wasm dependency to version 0.0.5 and clean up global type declarations

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,11 +1,5 @@
 import { EmbindModule } from '@luisbedoia/coolprop-wasm';
 
-declare module "@luisbedoia/coolprop-wasm" {
-  interface EmbindModule {
-    get_global_param_string?: (param: string) => string;
-  }
-}
-
 declare global {
   interface Window {
     CP: EmbindModule;
